4ba31bcb184c098b2832c34fe6f92a8362280842
aungkyawhtwe1998 <aungkyawhtwe.mdy49@gmail.com>
2021-12-26 17:23:55 +0630

    final one

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
     <div id="nav">
       <router-link :to="{ name: 'EventList' }">Events</router-link> |
       <router-link :to="{ name: 'About' }">About</router-link> |
-      <router-link :to="{ name: 'EventCreate' }">Create Event</router-link>
     </div>
     <router-view />
   </div>
